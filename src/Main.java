85e509f7b245a9a08c6005b07af765d9a70c06c6
uliaerosenko <eroshhenko03@gmail.com>
2024-04-23 10:59:30 +0300

    task corrected

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,16 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         // task 1
-        int yearsLeap = 2024;
-        if (isYearsLeap(yearsLeap)) {
-            System.out.println(yearsLeap + " високосный год");
+        int leapYears = 2024;
+        if (isYearsLeap(leapYears)) {
+            System.out.println(leapYears + " високосный год");
         } else {
-            System.out.println(yearsLeap + " не високосный год");
+            System.out.println(leapYears + " не високосный год");
         }
         // task 2
         printVersion(1,2002);
     }
+
     public static boolean isYearsLeap (int yearsLeap) {
         return yearsLeap >= 1584 && yearsLeap % 4 == 0 && yearsLeap % 100 != 0 || yearsLeap % 400 == 0;
     }
@@ -35,21 +36,24 @@ public class Main {
         }
         // task 3
 
-        distance(60);
-        distance(180);
-        distance(87);
-    }
-
-    public static void distance (int deliveryDistance) {
-        if (deliveryDistance >= 1 && deliveryDistance <= 20) {
-            System.out.println("Доставка занимает сутки");
-        } else if (deliveryDistance >= 21 && deliveryDistance <= 60) {
-            System.out.println("Доставка занимает двое суток");
-        } else if (deliveryDistance >= 61 && deliveryDistance <= 100) {
-            System.out.println("Доставка занимает трое суток");
-        } else if (deliveryDistance <= 100) {
+        int distance = 94;
+        int deliveryDays = calculateSum(distance);
+        if (deliveryDays >= 0 && deliveryDays <= 3) {
+            System.out.println("Доставка занимает " + deliveryDays + " дня");
         } else {
             System.out.println("Доставки нет");
+        }
+    }
+
+    public static int calculateSum (int deliveryDistance) {
+        if (deliveryDistance >= 1 && deliveryDistance <= 20) {
+            return 1;
+        } else if (deliveryDistance >= 21 && deliveryDistance <= 60) {
+            return 2;
+        } else if (deliveryDistance >= 61 && deliveryDistance <= 100) {
+            return 3;
+        } else {
+            return 4;
         }
     }
 }
